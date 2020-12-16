9263e88c7ddb848c541447ea82847121f9cb0243
mks2192 <manojsinghabc@gmail.com>
2020-12-16 20:29:29 +0530

    'changes'

--- SQLQueryT4 - Sub Query.sql
+++ SQLQueryT4 - Sub Query.sql
@@ -3,3 +3,12 @@
 -- AdventureWorks2012.bak
 
 USE AdventureWorks2012
+
+	SELECT DISTINCT SafetyStockLevel, ListPrice FROM Production.Product
+	WHERE ListPrice in (
+						SELECT Min(ListPrice) MinlistPrice
+						FROM Production.Product
+						Where --SafetyStockLevel > 100 and  
+						ListPrice > 0
+						GROUP BY 
+						SafetyStockLevel)
