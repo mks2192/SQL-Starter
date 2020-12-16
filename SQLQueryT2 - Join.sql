80942faf825717b31b73e20f5d96f8e2fed42418
mks2192 <manojsinghabc@gmail.com>
2020-12-16 19:02:47 +0530

    'changes'

--- SQLQueryT2 - Join.sql
+++ SQLQueryT2 - Join.sql
@@ -4,3 +4,25 @@
 
 USE AdventureWorks2012
 
+-- Inner Join
+		SELECT P.Name, P.Color, P.Weight, TH.Quantity, P.ListPrice - TH.ActualCost as Profit
+		FROM Production.Product P
+		INNER JOIN  -- same as JOIN
+		Production.TransactionHistory TH
+		ON 
+		P.ProductID = TH.ProductID
+
+-- Information Schema All Tables
+
+		SELECT * FROM INFORMATION_SCHEMA.TABLES
+
+-- Multiple Tables Joining
+
+		SELECT * FROM HumanResources.Employee E
+		INNER JOIN 
+		HumanResources.EmployeeDepartmentHistory EDH
+		ON
+		E.BusinessEntityID = EDH.BusinessEntityID
+		INNER JOIN
+		HumanResources.Department D
+		ON EDH.DepartmentID = D.DepartmentID
